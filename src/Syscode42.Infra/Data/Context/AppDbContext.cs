7f303e8b377e51f42b3555d4fbcc563417804ab5
black-adm <mattheuscontato17@gmail.com>
2025-11-09 17:00:48 -0300

    fix: infra layer and db context

---
+++ src/Syscode42.Infra/Data/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Syscode42.Business.Models.Products;
+using Syscode42.Business.Models.Suppliers;
+using System.Data.Entity;
+
+namespace Syscode42.Infra.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection") {}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
