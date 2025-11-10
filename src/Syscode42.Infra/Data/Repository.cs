461d10fb160aa6e27dd4d2a979e138a73e0aacea
black-adm <mattheuscontato17@gmail.com>
2025-11-09 23:38:00 -0300

    fix: generic abstract repository

---
+++ src/Syscode42.Infra/Data/Repository.cs
@@ -0,0 +1,68 @@
+﻿using Syscode42.Business.Core.Data;
+using Syscode42.Business.Core.Models;
+using Syscode42.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Syscode42.Infra.Data
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : Entity, new()
+    {
+        protected readonly AppDbContext Db;
+        protected readonly DbSet<TEntity> DbSet;
+
+        protected Repository()
+        {
+            Db = new AppDbContext();
+            DbSet = Db.Set<TEntity>();
+        }
+
+        public virtual async Task<List<TEntity>> GetAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> SearchAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task AddAsync(TEntity entity)
+        {
+            DbSet.Add(entity);
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            Db.Entry(entity).State = EntityState.Modified;
+            await SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(Guid id)
+        {
+            Db.Entry(new TEntity { Id = id }).State = EntityState.Deleted;
+            await SaveChangesAsync();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
