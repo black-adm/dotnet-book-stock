2b65defc7d4b658e2c06639ff138d26bbfb3be03
black-adm <mattheuscontato17@gmail.com>
2025-11-14 23:47:47 -0300

    fix: setup identiy in app mvc layer

---
+++ src/Syscode42.AppMvc/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Microsoft.Owin;
+using Owin;
+using Syscode42.AppMvc.App_Start;
+
+[assembly: OwinStartup(typeof(Syscode42.AppMvc.Startup))]
+namespace Syscode42.AppMvc
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			CultureConfig.RegisterCulture();
+		}
+	}
+}
