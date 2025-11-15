14f56ebbd97f2907d9393a05d0b73a138dd32709
black-adm <mattheuscontato17@gmail.com>
2025-11-14 21:12:05 -0300

    fix: product service base

---
+++ src/Syscode42.Business/Models/Products/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Syscode42.Business.Models.Products.Services
+{
+    public interface IProductService : IDisposable
+    {
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Guid productId);
+    }
+}
---
+++ src/Syscode42.Business/Models/Products/Services/ProductService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Syscode42.Business.Core.Services;
+using Syscode42.Business.Models.Products.Validations;
+
+namespace Syscode42.Business.Models.Products.Services
+{
+	public class ProductService : BaseService, IProductService
+	{
+		private readonly IProductRepository _productRepository;
+
+		public ProductService(IProductRepository productRepository)
+		{
+			_productRepository = productRepository;
+		}
+
+		public async Task AddAsync(Product product)
+		{
+			if (!ExecuteValidation(new ProductValidation(), product))
+				return;
+
+			await _productRepository.AddAsync(product);
+		}
+
+		public async Task UpdateAsync(Product product)
+		{
+			if (!ExecuteValidation(new ProductValidation(), product))
+				return;
+
+			await _productRepository.UpdateAsync(product);
+		}
+
+		public async Task DeleteAsync(Guid productId)
+		{
+			await _productRepository.DeleteAsync(productId);
+		}
+
+		public void Dispose()
+		{
+			_productRepository?.Dispose();
+		}
+	}
+}
