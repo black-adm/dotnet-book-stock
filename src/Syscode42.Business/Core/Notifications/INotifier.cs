ab002546397809602d7092947aae09128898a624
black-adm <mattheuscontato17@gmail.com>
2025-11-14 21:20:59 -0300

    fix: basis for the notification mechanism

---
+++ src/Syscode42.Business/Core/Notifications/INotifier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Syscode42.Business.Core.Notifications
+{
+    public interface INotifier
+    {
+        bool HasNotifications();
+        List<Notification> GetNotifications();
+        void Handle(Notification notification);
+	}
+}
---
+++ src/Syscode42.Business/Core/Notifications/Notifier.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Syscode42.Business.Core.Notifications
+{
+    public class Notifier : INotifier
+    {
+        private List<Notification> _notifications;
+
+        public Notifier()
+        {
+            _notifications = new List<Notification>();
+		}
+
+		public void Handle(Notification notification)
+		{
+			_notifications.Add(notification);
+		}
+
+		public List<Notification> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public bool HasNotifications()
+        {
+            return _notifications.Any();
+        }
+    }
+}
